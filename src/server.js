04f30b99b24d8c9f7c5bf970a55ca1e51dd14269
Felipe Marcon <felipeemarcon@gmail.com>
2019-04-09 22:16:26 -0300

    Install and config socket.io

--- src/server.js
+++ src/server.js
@@ -4,6 +4,15 @@ const path = require("path");
 
 const app = express();
 
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+io.on("connection", socket => {
+  socket.on("coonectRoom", box => {
+    socket.join(box);
+  });
+});
+
 mongoose.connect(
   "mongodb+srv://deploy:deploy@rocketbox-4vhin.mongodb.net/rocketbox?retryWrites=true",
   {
@@ -11,10 +20,16 @@ mongoose.connect(
   }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(require("./routes"));
 
-app.listen(3000);
+server.listen(3000);
